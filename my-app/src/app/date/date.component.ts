955cd41b66b616cdb7ff9a9a6d163308ab8d29e4
holus-bolus <ustinovoleksij@gmail.com>
2022-12-05 17:51:59 +0200

    Added events for elements

--- my-app/src/app/date/date.component.ts
+++ my-app/src/app/date/date.component.ts
@@ -1,16 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-date',
   templateUrl: './date.component.html',
   styleUrls: ['./date.component.scss'],
 })
-export class DateComponent implements OnInit{
-items = ['Angular','React','Vue','Bootstrap','Node.js'];
-  constructor() {
+export class DateComponent implements OnInit {
+  // loggedIn = true;
+  isCollapsed: boolean = true;
 
+  constructor() {}
+
+  // myEvent(event: any) {
+  //   console.log(event);
+  // }
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
