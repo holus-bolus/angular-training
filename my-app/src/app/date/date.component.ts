193cb6ad7cdcd133c4c9a4bd2dc20a33e4e8a501
holus-bolus <ustinovoleksij@gmail.com>
2022-12-05 16:28:54 +0200

    Added an gcore

--- my-app/src/app/date/date.component.ts
+++ my-app/src/app/date/date.component.ts
@@ -6,11 +6,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./date.component.scss'],
 })
 export class DateComponent implements OnInit{
-  message: string | undefined;
+items = ['Angular','React','Vue','Bootstrap','Node.js'];
   constructor() {
-    setInterval(() => {
-      this.message = new Date().toLocaleTimeString();
-    }, 1000);
+
   }
 
   ngOnInit(): void {
